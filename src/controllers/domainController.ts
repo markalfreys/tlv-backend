7c5adb2338d955951427afad55fce8ebe012002a
markalfreys <142733376+markalfreys@users.noreply.github.com>
2025-03-02 12:56:29 +0800

    Update domainController.ts

--- src/controllers/domainController.ts
+++ src/controllers/domainController.ts
@@ -7,22 +7,26 @@ export const getDomainDetails = async (req: Request, res: Response) => {
     try {
 
         const { domain } = req.body
-        
+
+        // Validate the domain input, return 400 if domain is not found and not wrong type
         if(!domain || typeof domain !== 'string') {
             res.status(400).json({ success: false, data: null, message: "Domain is required" });
         } 
 
+        // Fetch WHOIS data
         const whoisData = await fetchWhoisData(domain as string) as { success: boolean, message: string, data: WhoisDataType  };
 
+        // return 400 status if fetching info on WHOIS API not successful
         if(!whoisData.success) {
             res.status(400).json({ success: false, data: null, message: whoisData.message });
         }
 
+         // return 400 status if fetching is success but there is no data received
         if(!whoisData.data) {
             res.status(400).json({ success: false, data: null, message: "No data found" });
         }
 
-    
+        // structure the data needed
         const domainDetailsData = {
            domainInformation: {
                 domainName: whoisData.data.WhoisRecord.domainName || '',
@@ -40,6 +44,7 @@ export const getDomainDetails = async (req: Request, res: Response) => {
             }
         };
 
+        // return the data with message if successfully fetched
         res.status(200).json({ success: true, data: domainDetailsData, message: "Domain details fetched successfully" });
 
     } catch (error) {
