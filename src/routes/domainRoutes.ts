0b3dbd68a6152a2f591c2184ffbf27b6fd87c3a4
markalfreys <142733376+markalfreys@users.noreply.github.com>
2025-03-02 12:50:03 +0800

    Update domainRoutes.ts

--- src/routes/domainRoutes.ts
+++ src/routes/domainRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { getDomainDetails } from "../controllers/domainController.js";
 
-
+// Routes for /api/domain
 export default (router: express.Router) => {
-    router.post('/', getDomainDetails);
+    router.post('/', getDomainDetails); //  Handle domain search via POST request
 };
