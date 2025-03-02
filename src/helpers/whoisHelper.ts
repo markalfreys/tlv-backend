1012d7351a68da81605608aa7399c05e9a3165b8
markalfreys <142733376+markalfreys@users.noreply.github.com>
2025-03-02 12:52:32 +0800

    Update whoisHelper.ts

--- src/helpers/whoisHelper.ts
+++ src/helpers/whoisHelper.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import { ENV } from '../config/env.js';
 
+// Fetches WHOIS data for a given domain from an external API.
 export const fetchWhoisData = async (domain: string) => {
-    const url = ENV.WHOIS_API_URL;
+    const url = ENV.WHOIS_API_URL; // API endpoint from environment variables
 
     try {
         const { data } = await axios.post(
             url, 
             { 
-                apiKey: ENV.WHOIS_API_KEY,
-                domainName: domain, 
+                apiKey: ENV.WHOIS_API_KEY, // API key for authentication
+                domainName: domain,        // Domain name to query
                 outputFormat: 'json' 
             },{
                 headers: {
@@ -17,8 +18,12 @@ export const fetchWhoisData = async (domain: string) => {
                 },
             }
         );
+
+        // return the data if fetch successfully
         return { success: true,  data, message: "Domain details fetched successfully" };
     } catch (error) {
+
+        // return success to false if data is not fetched
         return { success: false, data: null, message: "Error fetching domain details" };
     }
 };
